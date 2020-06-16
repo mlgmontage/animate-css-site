e402eefa1a1bcd924d5e76e376e66a382bba38b3
mlgmontage <kettleofrassel1@gmail.com>
2020-06-16 17:24:33 +0500

    1.0 version

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("hello");
+const container = document.getElementById("container");
+container.style.height = window.innerHeight + "px"; // fit to screen size
+
+const animated = document.getElementById("animated");
+const triggers = document.getElementsByClassName("trigger");
+
+for (let i = 0; i < triggers.length; i++) {
+  // trigger the animation
+  triggers[i].addEventListener("click", () => {
+    const animation_name = triggers[i].name;
+    animated.classList.add("animate__animated", `animate__${animation_name}`);
+
+    // remove class after animation ended
+    animated.addEventListener("animationend", () => {
+      animated.classList.remove(
+        "animate__animated",
+        `animate__${animation_name}`
+      );
+    });
+  });
+}
